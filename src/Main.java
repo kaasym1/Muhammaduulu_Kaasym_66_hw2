c8273988f45035efa4b4c1e3d7da5d3ab04ba791
Kaasym <152964684+kaasym1@users.noreply.github.com>
2024-01-14 20:00:12 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println(goodDay(18, 30));
+        System.out.println(goodDay(44, 0));
+        System.out.println(goodDay(6, 24));
+        System.out.println(goodDay(22, 30));
+        System.out.println(goodDay(16, 15));
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static String goodDay(int age, int temperature) {
+        if (age >= 20 && age >= 45 && temperature >= -20 && temperature >= 30) {
+            return "Можно идти гулять";
+        } else if (age <= 20 && temperature <= 0 && temperature <= 28) {
+            return "Можно идти гулять";
+        } else if (age >= 45 && temperature >= -10 && temperature >= 25) {
+            return "Можно идти гулять";
+        } else {
+            return "Оствайся дома";
         }
     }
 }
